8d82c10ee6cc0ad54acfea50e9f53bccdcdc88bc
Oleg Rubanov <165693738+orubanov@users.noreply.github.com>
2024-05-03 02:02:24 +0300

    Add files via upload

---
+++ hw16_1.py
@@ -0,0 +1,6 @@
+def function1():
+    print('Hello, world')
+
+
+def function2():
+    print('Hello, world')
---
+++ hw16_2.py
@@ -0,0 +1,7 @@
+from hw16_1 import function1, function2
+import hw16_1 as hw
+
+function1()
+function2()
+hw.function1()
+hw.function2()
